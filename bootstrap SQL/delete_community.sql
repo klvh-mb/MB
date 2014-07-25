3ed2dad6a8d810ce94ecf8d6896424a2ab846df6
keithlei <keithlei01@gmail.com>
2014-07-26 01:10:32 +0800

    soft delete community sql

--- bootstrap SQL/delete_community.sql
+++ bootstrap SQL/delete_community.sql
@@ -1,3 +1,3 @@
-update Comment set deleted = true where socialObject in (select p.id from Post p where p.community_id = 32);
-update Post set deleted = true where community_id = 32;
-update Community set deleted = true where id = 32;
+update Comment set deleted = true where socialObject in (select p.id from Post p where p.community_id = );
+update Post set deleted = true where community_id = ;
+update Community set deleted = true where id = ;
