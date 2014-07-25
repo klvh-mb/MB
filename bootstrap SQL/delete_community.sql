275fbb1c821811af7246dc5893ffd58365a79196
keithlei <keithlei01@gmail.com>
2014-07-26 02:30:35 +0800

    delete comm sql

--- bootstrap SQL/delete_community.sql
+++ bootstrap SQL/delete_community.sql
@@ -1,3 +1,4 @@
 update Comment set deleted = true where socialObject in (select p.id from Post p where p.community_id = );
 update Post set deleted = true where community_id = ;
 update Community set deleted = true where id = ;
+update UserCommunityAffinity set newsfeedEnabled = false where communityId = ;
