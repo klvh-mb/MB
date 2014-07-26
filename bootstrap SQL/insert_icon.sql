2494c6dfc26fdd4ff20efbc9aea81341aa0d1168
keithlei <keithlei01@gmail.com>
2014-07-27 00:58:18 +0800

    community icons

---
+++ bootstrap SQL/insert_icon.sql
@@ -0,0 +1,8 @@
+insert into Icon (iconType,info,name,url) values ('COMMUNITY_GENERAL','','grad_hat','/assets/app/images/general/icons/community/grad_hat.png');
+insert into Icon (iconType,info,name,url) values ('COMMUNITY_GENERAL','','sports','/assets/app/images/general/icons/community/sports.png');
+insert into Icon (iconType,info,name,url) values ('COMMUNITY_GENERAL','','shirt','/assets/app/images/general/icons/community/shirt.png');
+insert into Icon (iconType,info,name,url) values ('COMMUNITY_GENERAL','','bath','/assets/app/images/general/icons/community/bath.png');
+insert into Icon (iconType,info,name,url) values ('COMMUNITY_GENERAL','','mom','/assets/app/images/general/icons/community/mom.png');
+insert into Icon (iconType,info,name,url) values ('COMMUNITY_GENERAL','','dad','/assets/app/images/general/icons/community/dad.png');
+insert into Icon (iconType,info,name,url) values ('COMMUNITY_GENERAL','','home','/assets/app/images/general/icons/community/home.png');
+insert into Icon (iconType,info,name,url) values ('COMMUNITY_GENERAL','','palette','/assets/app/images/general/icons/community/paletter.png');
