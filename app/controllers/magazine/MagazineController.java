4dc3fd2fa5fc743b2c17d420eba700a74480446b
keithlei <keithlei01@gmail.com>
2014-09-03 20:17:53 +0800

    fix build

--- app/controllers/magazine/MagazineController.java
+++ app/controllers/magazine/MagazineController.java
@@ -1,6 +1,5 @@
 package controllers.magazine;
 
-import play.Play;
 import play.db.jpa.Transactional;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -8,8 +7,10 @@ import play.mvc.Result;
 public class MagazineController extends Controller {
     private static final play.api.Logger logger = play.api.Logger.apply(MagazineController.class);
 
-    public static final String APPLICATION_BASE_URL = 
-            Play.application().configuration().getString("application.baseUrl");
+    @Transactional
+    public static Result redirect() {
+        return index();
+    }
     
 	@Transactional
 	public static Result index() {
