a167307c549645c0548a499265ac1941d859e55e
keithlei <keithlei01@gmail.com>
2014-11-25 20:13:41 +0800

    reply and time icons for post

--- app/common/model/ZodiacYear.java
+++ app/common/model/ZodiacYear.java
@@ -3,18 +3,18 @@ package common.model;
 import common.model.TargetYear.Zodiac;
 
 /**
- * update community set targetinginfo='2008_RAT' where id=1;
- * update community set targetinginfo='2009_OX' where id=2;
- * update community set targetinginfo='2010_TIGER' where id=3;
- * update community set targetinginfo='2011_RABBIT' where id=4;
- * update community set targetinginfo='2012_DRAGON' where id=5;
- * update community set targetinginfo='2013_SNAKE' where id=6;
- * update community set targetinginfo='2014_HORSE' where id=7;
- * update community set targetinginfo='2015_GOAT' where id=8;
- * update community set targetinginfo='2016_MONKEY' where id=9;
- * update community set targetinginfo='2017_ROOSTER' where id=10;
- * update community set targetinginfo='2018_DOG' where id=11;
- * update community set targetinginfo='2019_PIG' where id=12;
+ * update community set targetinginfo='2008_RAT' where targetinginfo='RAT';
+ * update community set targetinginfo='2009_OX' where targetinginfo='OX';
+ * update community set targetinginfo='2010_TIGER' where targetinginfo='TIGER';
+ * update community set targetinginfo='2011_RABBIT' where targetinginfo='RABBIT';
+ * update community set targetinginfo='2012_DRAGON' where targetinginfo='DRAGON';
+ * update community set targetinginfo='2013_SNAKE' where targetinginfo='SNAKE';
+ * update community set targetinginfo='2014_HORSE' where targetinginfo='HORSE';
+ * update community set targetinginfo='2015_GOAT' where targetinginfo='GOAT';
+ * update community set targetinginfo='2016_MONKEY' where targetinginfo='MONKEY';
+ * update community set targetinginfo='2017_ROOSTER' where targetinginfo='ROOSTER';
+ * update community set targetinginfo='2018_DOG' where targetinginfo='DOG';
+ * update community set targetinginfo='2019_PIG' where targetinginfo='PIG';
  */
 public class ZodiacYear {
     
