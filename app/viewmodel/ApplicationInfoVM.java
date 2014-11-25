bc2470cee05a4a77d3d96606361e9dd977eb71c1
keithlei <keithlei01@gmail.com>
2014-11-25 21:51:25 +0800

    game page promo code

--- app/viewmodel/ApplicationInfoVM.java
+++ app/viewmodel/ApplicationInfoVM.java
@@ -4,10 +4,8 @@ import org.codehaus.jackson.annotate.JsonProperty;
 
 public class ApplicationInfoVM {
     @JsonProperty("baseUrl") private String baseUrl;
-    @JsonProperty("mobile") private boolean isMobileUser;
     
-    public ApplicationInfoVM(String baseUrl, boolean isMobileUser) {
+    public ApplicationInfoVM(String baseUrl) {
         this.baseUrl = baseUrl;
-        this.isMobileUser = isMobileUser;
     }
 }
