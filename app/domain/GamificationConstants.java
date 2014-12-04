d103d0b436c6563509e84e60ab723f78ea353b51
Victor <vichoty@yahoo.com>
2014-12-04 16:29:06 +0800

    Revised game points scheme

--- app/domain/GamificationConstants.java
+++ app/domain/GamificationConstants.java
@@ -15,16 +15,16 @@ public interface GamificationConstants {
     /**
      * Points
      * Conversion Rate = 0.007 (as of Nov 29th, 2014)
-     * i.e. $50 gift = 50 / 0.007 = 7143 points (~274 days to get with full activity)
+     * i.e. $50 gift = 50 / 0.007 = 7143 points (~285 days to get with full activity)
      */
     public static final int POINTS_SIGNUP = 50;
     public static final int POINTS_UPLOAD_PROFILE_PHOTO = 150;
-    public static final int POINTS_SIGNIN = 6;
+    public static final int POINTS_SIGNIN = 5;
     public static final int POINTS_POST = 2;
     public static final int POINTS_COMMENT = 2;
     public static final int POINTS_LIKE = 0;             // not open yet
 
-    public static final int POINTS_REFERRAL_SIGNUP = 280;
+    public static final int POINTS_REFERRAL_SIGNUP = 500;
 
     /**
      * Limit on Points accounting
