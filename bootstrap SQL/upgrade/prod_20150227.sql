25c4e34d485b48b1883785708c63b476c3b79dea
keithlei <keithlei01@gmail.com>
2015-03-02 16:29:49 +0800

    pn tips

--- bootstrap SQL/upgrade/prod_20150227.sql
+++ bootstrap SQL/upgrade/prod_20150227.sql
@@ -6,6 +6,13 @@ update community set deleted=true where id=209;
 update community set deleted=true where id=212;
 update community set deleted=true where id=214;
 update community set deleted=true where id=215;
+update post set deleted=true where community_id=40;
+update post set deleted=true where community_id=41;
+update post set deleted=true where community_id=42;
+update post set deleted=true where community_id=209;
+update post set deleted=true where community_id=212;
+update post set deleted=true where community_id=214;
+update post set deleted=true where community_id=215;
 update community_communitycategory set communitycategories_id=7 where community_id=37;
 update community_communitycategory set communitycategories_id=7 where community_id=38;
 update community_communitycategory set communitycategories_id=7 where community_id=39;
@@ -14,3 +21,4 @@ update communitycategory set deleted=true where id=8;
 
 /* Remove district comms */
 update community set deleted=true where id>=13 and id<=30;
+update post set deleted=true where community_id>=13 and community_id<=30;
