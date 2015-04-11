2dad120ff9db4a4952995ede7d8b479e0e16de65
Victor <vichoty@yahoo.com>
2015-04-11 21:17:20 +0800

    Added school logo icons.

--- app/models/PreNursery.java
+++ app/models/PreNursery.java
@@ -26,6 +26,7 @@ public class PreNursery extends SocialObject implements Likeable, Commentable {
 
     // name is inherited
     public String nameEn;
+    public String icon;
 
     public String phoneText;
     public String url;
