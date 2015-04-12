5a7b8cfcabfc12842ecb7dc215d50a7cca557807
Victor <vichoty@yahoo.com>
2015-04-12 09:26:20 +0800

    Added more school logos.

--- bootstrap SQL/kgicon.sql
+++ bootstrap SQL/kgicon.sql
@@ -6,20 +6,25 @@ update community set icon = '/assets/app/images/general/icons/community/kg/elh.p
 update community set icon = '/assets/app/images/general/icons/community/kg/funful.png' where id in (select communityId from PreNursery where nameEn like '%Funful%');
 update community set icon = '/assets/app/images/general/icons/community/kg/hkpresch.png' where id in (select communityId from PreNursery where nameEn like '%Ascot%');
 update community set icon = '/assets/app/images/general/icons/community/kg/holytrinity.png' where id in (select communityId from PreNursery where nameEn like 'Holy Trinity%');
+update community set icon = '/assets/app/images/general/icons/community/kg/kcis.png' where id in (select communityId from PreNursery where nameEn like 'Kiangsu%Non%');
+update community set icon = '/assets/app/images/general/icons/community/kg/kclocal.png' where id in (select communityId from PreNursery where nameEn like 'Kiangsu%(Local%');
 update community set icon = '/assets/app/images/general/icons/community/kg/keenmind.png' where id in (select communityId from PreNursery where nameEn like 'Keen Mind%');
 update community set icon = '/assets/app/images/general/icons/community/kg/klcbchn.png' where id in (select communityId from PreNursery where nameEn like 'Kowloon City Baptist Church%');
 update community set icon = '/assets/app/images/general/icons/community/kg/kllingliang.png' where id in (select communityId from PreNursery where nameEn like 'Kowloon Ling Liang%');
 update community set icon = '/assets/app/images/general/icons/community/kg/learninghabitat.png' where id in (select communityId from PreNursery where nameEn like 'Learning Habitat%');
 update community set icon = '/assets/app/images/general/icons/community/kg/ltlingliang.png' where id in (select communityId from PreNursery where nameEn like 'Lam Tin Ling Liang%');
 update community set icon = '/assets/app/images/general/icons/community/kg/rhs.png' where id in (select communityId from PreNursery where nameEn like 'Rosar%');
+update community set icon = '/assets/app/images/general/icons/community/kg/sjc.png' where id in (select communityId from PreNursery where nameEn like 'St Johannes%');
 update community set icon = '/assets/app/images/general/icons/community/kg/spckg.png' where id in (select communityId from PreNursery where nameEn like 'HKSKH St Peter%');
 update community set icon = '/assets/app/images/general/icons/community/kg/spn.png' where id in (select communityId from PreNursery where nameEn like '%Paul%s Day%');
 update community set icon = '/assets/app/images/general/icons/community/kg/stcatherine.png' where id in (select communityId from PreNursery where nameEn like '%St Catherin%');
 update community set icon = '/assets/app/images/general/icons/community/kg/think.png' where id in (select communityId from PreNursery where nameEn like '%Think%');
+update community set icon = '/assets/app/images/general/icons/community/kg/tutortime.png' where id in (select communityId from PreNursery where nameEn like 'Tutor Time%');
 update community set icon = '/assets/app/images/general/icons/community/kg/victoria.png' where id in (select communityId from PreNursery where nameEn like '%Victoria%');
 update community set icon = '/assets/app/images/general/icons/community/kg/wisely.png' where id in (select communityId from PreNursery where nameEn like 'Wisely%');
 update community set icon = '/assets/app/images/general/icons/community/kg/yanoitong.png' where id in (select communityId from PreNursery where nameEn like 'Yan O%');
 update community set icon = '/assets/app/images/general/icons/community/kg/york.png' where id in (select communityId from PreNursery where nameEn like 'York English%');
+update community set icon = '/assets/app/images/general/icons/community/kg/zenith.png' where id in (select communityId from PreNursery where nameEn like 'Zenith%');
 
 
 update PreNursery set icon = '/assets/app/images/general/icons/community/kg/boxhill.png' where nameEn like '%Box Hill%';
@@ -30,19 +35,26 @@ update PreNursery set icon = '/assets/app/images/general/icons/community/kg/elh.
 update PreNursery set icon = '/assets/app/images/general/icons/community/kg/funful.png' where nameEn like '%Funful%';
 update PreNursery set icon = '/assets/app/images/general/icons/community/kg/hkpresch.png' where nameEn like '%Ascot%';
 update PreNursery set icon = '/assets/app/images/general/icons/community/kg/holytrinity.png' where nameEn like 'Holy Trinity%';
+update PreNursery set icon = '/assets/app/images/general/icons/community/kg/kcis.png' where nameEn like 'Kiangsu%Non%';
+update PreNursery set icon = '/assets/app/images/general/icons/community/kg/kclocal.png' where nameEn like 'Kiangsu%(Local%';
 update PreNursery set icon = '/assets/app/images/general/icons/community/kg/keenmind.png' where nameEn like 'Keen Mind%';
 update PreNursery set icon = '/assets/app/images/general/icons/community/kg/klcbchn.png' where nameEn like 'Kowloon City Baptist Church%';
 update PreNursery set icon = '/assets/app/images/general/icons/community/kg/kllingliang.png' where nameEn like 'Kowloon Ling Liang%';
 update PreNursery set icon = '/assets/app/images/general/icons/community/kg/learninghabitat.png' where nameEn like 'Learning Habitat%';
 update PreNursery set icon = '/assets/app/images/general/icons/community/kg/ltlingliang.png' where nameEn like 'Lam Tin Ling Liang%';
 update PreNursery set icon = '/assets/app/images/general/icons/community/kg/rhs.png' where nameEn like 'Rosar%';
+update PreNursery set icon = '/assets/app/images/general/icons/community/kg/sjc.png' where nameEn like 'St Johannes%';
 update PreNursery set icon = '/assets/app/images/general/icons/community/kg/spckg.png' where nameEn like 'HKSKH St Peter%';
 update PreNursery set icon = '/assets/app/images/general/icons/community/kg/spn.png' where nameEn like '%Paul%s Day%';
 update PreNursery set icon = '/assets/app/images/general/icons/community/kg/stcatherine.png' where nameEn like '%St Catherin%';
-update PreNursery set icon = '/assets/app/images/general/icons/community/kg/think.gif' where nameEn like '%Think%';
+update PreNursery set icon = '/assets/app/images/general/icons/community/kg/think.png' where nameEn like '%Think%';
+update PreNursery set icon = '/assets/app/images/general/icons/community/kg/tutortime.png' where nameEn like 'Tutor Time%';
 update PreNursery set icon = '/assets/app/images/general/icons/community/kg/victoria.png' where nameEn like '%Victoria%';
 update PreNursery set icon = '/assets/app/images/general/icons/community/kg/wisely.png' where nameEn like 'Wisely%';
 update PreNursery set icon = '/assets/app/images/general/icons/community/kg/yanoitong.png' where nameEn like 'Yan O%';
 update PreNursery set icon = '/assets/app/images/general/icons/community/kg/york.png' where nameEn like 'York English%';
+update PreNursery set icon = '/assets/app/images/general/icons/community/kg/zenith.png' where nameEn like 'Zenith%';
+
+
 
 commit;
