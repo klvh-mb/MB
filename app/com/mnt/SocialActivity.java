3761b66e8ddd6ab341db468ebbca87c68158ea0a
Victor <vichoty@yahoo.com>
2015-05-03 09:24:48 +0800

    Fixed up Notifications on comment.

--- app/com/mnt/SocialActivity.java
+++ app/com/mnt/SocialActivity.java
@@ -529,17 +529,15 @@ public class SocialActivity {
 
                     String shortTitle = post.getShortenedTitle();
                     String shortBody = comment.getShortenedBody();
-                    String genMsgEnd = " 回應了話題 - \""+shortTitle+"\"";
-                    String yrMsgEnd = " 回應了您的話題 - \""+shortTitle+"\"";
+                    String genMsgEnd = " 回應了話題: \""+shortTitle+"\"";
+                    String yrMsgEnd = " 回應了您的話題: \""+shortTitle+"\"";
 
-                    // fan-out, or just to the post owner
-                    Collection<Long> recipientIds;
+                    // add owner and comment users
+                    Set<Long> recipientIds = post.getCommentUserIdsOfPost();
+                    recipientIds.add(post.owner.id);
+                    // fan-out if applicable
                     if (isSendToAllMembers(community)) {
-                        recipientIds = community.getMemberIds();
-                    } else {
-                        Set<Long> commentUserIds = post.getCommentUserIdsOfPost();
-                        commentUserIds.add(post.owner.id);
-                        recipientIds = commentUserIds;
+                        recipientIds.addAll(community.getMemberIds());
                     }
 
                     for (Long recipientId : recipientIds) {
