4f33c60fcdbb97f9ac4331bced3b7c203f8f0963
Victor <vichoty@yahoo.com>
2015-07-01 19:13:16 +0800

    Modified app dates sql.

--- bootstrap SQL/kg_dates_2015.sql
+++ bootstrap SQL/kg_dates_2015.sql
@@ -1,7 +1,7 @@
-UPDATE Kindergarten set applicationDateText='8月上網睇, 9月1日開始Download Form' WHERE id=556 AND name='九龍靈糧堂幼稚園';
-UPDATE Kindergarten set applicationDateText='12月上網睇,1月開始報' WHERE id=695 AND name='劍鳴幼稚園';
-UPDATE Kindergarten set applicationDateText='8月尾9月頭上網睇,9月初招生' WHERE id=676 AND name='國際英文幼稚園(羅福道)';
+UPDATE Kindergarten set applicationDateText='8月上網睇, 9月1日開始Download Form' , applicationDate='2015-09-01' WHERE id=556 AND name='九龍靈糧堂幼稚園';
+UPDATE Kindergarten set applicationDateText='12月上網睇,1月開始報' , applicationDate='2016-01-31' WHERE id=695 AND name='劍鳴幼稚園';
+UPDATE Kindergarten set applicationDateText='8月尾9月頭上網睇,9月初招生' , applicationDate='2015-09-15' WHERE id=676 AND name='國際英文幼稚園(羅福道)';
 UPDATE Kindergarten set applicationDateText='8月24日 - 9月6日, 2015' , openDayText='簡介會 8月21日, 2015' , applicationDate='2015-08-24' WHERE id=478 AND name='崇真小學暨幼稚園';
-UPDATE Kindergarten set applicationDateText='8月網上報名' WHERE id=542 AND name='香港靈糧堂幼稚園';
-UPDATE Kindergarten set applicationDateText='9月上網睇, 10月開始報' WHERE id=711 AND name='根德園幼稚園';
-UPDATE Kindergarten set applicationDateText='8月開始報' WHERE id=44 AND name='偉思幼稚園';
+UPDATE Kindergarten set applicationDateText='8月網上報名' , applicationDate='2015-08-31' WHERE id=542 AND name='香港靈糧堂幼稚園';
+UPDATE Kindergarten set applicationDateText='9月上網睇, 10月開始報' , applicationDate='2015-10-31' WHERE id=711 AND name='根德園幼稚園';
+UPDATE Kindergarten set applicationDateText='8月開始報' , applicationDate='2015-08-31' WHERE id=44 AND name='偉思幼稚園';
