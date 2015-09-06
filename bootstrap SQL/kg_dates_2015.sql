002e8ad1112b66a97d73b26ee33b8728a553bce7
Victor <vichoty@yahoo.com>
2015-09-06 09:50:00 +0800

    Updated sql scripts.

--- bootstrap SQL/kg_dates_2015.sql
+++ bootstrap SQL/kg_dates_2015.sql
@@ -1,7 +1,34 @@
-UPDATE Kindergarten set applicationDateText='8月上網睇, 9月1日開始Download Form' , applicationDate='2015-09-01' WHERE id=556 AND name='九龍靈糧堂幼稚園';
+-- TODO
 UPDATE Kindergarten set applicationDateText='12月上網睇,1月開始報' , applicationDate='2016-01-31' WHERE id=695 AND name='劍鳴幼稚園';
-UPDATE Kindergarten set applicationDateText='8月尾9月頭上網睇,9月初招生' , applicationDate='2015-09-15' WHERE id=676 AND name='國際英文幼稚園(羅福道)';
-UPDATE Kindergarten set applicationDateText='8月24日 - 9月6日, 2015' , openDayText='簡介會 8月21日, 2015' , applicationDate='2015-08-24' WHERE id=478 AND name='崇真小學暨幼稚園';
-UPDATE Kindergarten set applicationDateText='8月網上報名' , applicationDate='2015-08-31' WHERE id=542 AND name='香港靈糧堂幼稚園';
-UPDATE Kindergarten set applicationDateText='9月上網睇, 10月開始報' , applicationDate='2015-10-31' WHERE id=711 AND name='根德園幼稚園';
-UPDATE Kindergarten set applicationDateText='8月開始報' , applicationDate='2015-08-31' WHERE id=44 AND name='偉思幼稚園';
+UPDATE Kindergarten set applicationDateText='於9月7日瀏覽「入學申請」版頁' , applicationDate='2015-09-07' WHERE id=676 AND name='國際英文幼稚園(羅福道)';
+UPDATE Kindergarten set applicationDateText='於9月7日瀏覽「入學申請」版頁' , applicationDate='2015-09-07' WHERE id=677 AND name='國際英文幼稚園(雅息士道)';
+UPDATE Kindergarten set applicationDateText='9月30日網站內公佈, 10月開始報' , applicationDate='2015-10-31' WHERE id=711 AND name='根德園幼稚園';
+UPDATE Kindergarten set applicationDateText='10月下旬上網睇',applicationDate='2015-10-31' WHERE id=250 AND name='天主教聖瑪加利大幼稚園';
+
+UPDATE Kindergarten set applicationDateText='2015年8月24日 - 9月6日' , openDayText='簡介會 2015年8月21日' , applicationDate='2015-08-24' WHERE id=478 AND name='崇真小學暨幼稚園';
+UPDATE Kindergarten set applicationDateText='2015年8月1日 - 10月15日' , applicationDate='2015-10-15' WHERE id in (59,66,141,387);
+UPDATE Kindergarten set openDayText='簡介會 2015年8月22日' WHERE id = 975;
+UPDATE Kindergarten set applicationDateText='2015年10月31日截止', applicationDate='2015-10-31' WHERE id=783 and name like '聖雅各福群會寶翠園%';
+
+UPDATE Kindergarten set applicationDateText='索取報名表: 2015年 9月11日(9-11am & 2-4pm), 9月12日(9am-12pm). 遞交報名表: 2015年 9月19日(9am-12pm), 9月21日(9-11am & 2-4pm)', applicationDate='2015-09-21' WHERE name like '%方方樂趣%';
+UPDATE Kindergarten set applicationDateText='2015年9月4日 9pm開始 (只接受網上報名)' , applicationDate='2015-09-04' WHERE id=542 AND name='香港靈糧堂幼稚園';
+UPDATE Kindergarten set applicationDateText='2015年9月4日 9pm開始 (只接受網上報名)' , applicationDate='2015-09-04' WHERE id=544 AND name='馬鞍山靈糧幼稚園';
+UPDATE Kindergarten set applicationDateText='2015年9月4日 9pm開始 (只接受網上報名)' , applicationDate='2015-09-04' WHERE id=545 AND name='藍田靈糧幼稚園';
+UPDATE Kindergarten set applicationDateText='2015年9月4日 9pm開始 (只接受網上報名)' , applicationDate='2015-09-04' WHERE id=547 AND name='鑽石山靈糧幼稚園';
+UPDATE Kindergarten set openDayText='簡介會 2015年10月10日',applicationDateText='2015年9月1日 - 9月15日 3pm止',applicationDate='2015-09-15' WHERE id=556 AND name='九龍靈糧堂幼稚園';
+UPDATE Kindergarten set applicationDateText='2015年10月26日 - 10月31日 12:30pm止',applicationDate='2015-10-31' WHERE id=879 AND name='聖三一中心幼稚園';
+UPDATE Kindergarten set openDayText='簡介會 2015年10月17日',applicationDateText='2015年10月6日 - 10月19日 (只設網上報名)', applicationDate='2015-10-19' WHERE name='香港培道小學幼稚園';
+UPDATE Kindergarten set applicationDateText='2015年9月30日止',applicationDate='2015-09-30' WHERE id=539 AND name='聖若望英文書院';
+UPDATE Kindergarten set applicationDateText='2015年9月30日止',applicationDate='2015-09-30' WHERE id=823 AND name='聖若望英文書院';
+UPDATE Kindergarten set applicationDateText='2015年9月15日9:30am - 9月30日11:55pm',applicationDate='2015-09-30' WHERE id=150 AND name='基督教中心幼稚園';
+UPDATE Kindergarten set applicationDateText='2015年9月15日 - 9月30日',applicationDate='2015-09-30' WHERE id=719 AND name='基督教中心幼稚園(油塘)';
+
+UPDATE Kindergarten set applicationDateText='索取報名表: 2015年8月24日 - 9月19日. 報名日期: 2015年9月16日 - 9月19日',applicationDate='2015-09-19' WHERE id=831 AND name='合一堂單家傳紀念幼稚園';
+UPDATE Kindergarten set applicationDateText='2016年1月11日 - 1月22日',applicationDate='2016-01-22' WHERE id not in (167) and name like '朗思國際%';
+UPDATE Kindergarten set applicationDateText='2015年8月10日 - 10月23日',applicationDate='2015-10-23' WHERE id=44 and name='偉思幼稚園';
+UPDATE Kindergarten set applicationDateText='下載報名表: 2015年9月14日(9:30am) - 9月20日. 遞交日期: 2015年9月21日 - 9月22日(9:30-11:30am & 1:30-4pm)',applicationDate='2015-09-22' WHERE id=133 and name='協恩中學附屬幼稚園';
+
+UPDATE Kindergarten set applicationDateText='派發報名表: 2015年9月5日. 報名日期: 2015年9月19日 - 9月23日', applicationDate='2015-09-23' WHERE name like'嘉德麗幼稚園%';
+
+
+
--- bootstrap SQL/pn_dates_2015.sql
+++ bootstrap SQL/pn_dates_2015.sql
@@ -1,14 +1,33 @@
-UPDATE PreNursery set applicationDateText='10月上網睇,11月1日開始Download Form' , applicationDate='2015-11-01' WHERE id=43 AND name='九龍靈糧堂幼兒園';
+-- TODO
 UPDATE PreNursery set applicationDateText='12月上網睇,1月開始報' , applicationDate='2016-01-31' WHERE id=205 AND name='劍鳴幼稚園';
-UPDATE PreNursery set applicationDateText='3月1日, 2016' , applicationDate='2016-03-01' WHERE id=44 AND name='聖保祿幼兒園';
 UPDATE PreNursery set applicationDateText='9月上網睇,1月開始報' , applicationDate='2016-01-31' WHERE id=148 AND name='基督教中心幼稚園';
-UPDATE PreNursery set applicationDateText='8月上網睇,9月開始報' , openDayText='10月' , applicationDate='2015-09-30' WHERE id=258 AND name='嘉德麗幼稚園';
+UPDATE PreNursery set applicationDateText='9月上網睇,11月7日拎Form' , applicationDate='2015-11-07' WHERE id=105 AND name='樂基幼兒學校(駿景園)';
+UPDATE PreNursery set applicationDateText='12月上網睇,1月報名' , applicationDate='2016-01-31' WHERE id=195 AND name='香港聖公會青山聖彼得堂青雲路幼稚園';
+UPDATE PreNursery set applicationDateText='於9月7日瀏覽「入學申請」版頁', applicationDate='2015-09-07' WHERE name='國際英文幼稚園';
+
+UPDATE PreNursery set applicationDateText='2016年3月1日' , applicationDate='2016-03-01' WHERE id=44 AND name='聖保祿幼兒園';
 UPDATE PreNursery set applicationDateText='冇指定日子, 依家可申請, 要mark 低申請2016/17年' , applicationDate='2016-12-31' WHERE id=466 AND name='約克英文小學暨幼稚園(九龍塘)';
 UPDATE PreNursery set applicationDateText='3月17日, 2015' , openDayText='過左' , applicationDate='2015-03-17' WHERE id=73 AND name='博士山(香港)國際幼稚園';
-UPDATE PreNursery set applicationDateText='9月上網睇,11月7日拎Form' , applicationDate='2015-11-07' WHERE id=105 AND name='樂基幼兒學校(駿景園)';
 UPDATE PreNursery set applicationDateText='夠1歲就可以報名' , applicationDate='2016-12-31' WHERE id=437 AND name='明雅國際幼兒學校';
-UPDATE PreNursery set applicationDateText='12月上網睇,1月報名' , applicationDate='2016-01-31' WHERE id=195 AND name='香港聖公會青山聖彼得堂青雲路幼稚園';
 UPDATE PreNursery set applicationDateText='夠1歲就報得' , openDayText='有,10月打去問' , applicationDate='2016-12-31' WHERE id=234 AND name='基督教香港信義會頌安幼兒學校';
 UPDATE PreNursery set applicationDateText='8月29日 - 9月12日, 2015' , openDayText='冇OPENDAY, 但可預約參觀, 星期1-5 1430-1600' , applicationDate='2015-08-29' WHERE id=282 AND name='綠茵英文(國際)幼稚園(常寧路)';
 UPDATE PreNursery set applicationDateText='夠1歲半就可以報名,全年申請,有位就可以Interview' , applicationDate='2016-12-31' WHERE id=428 AND name='基督教宣道會沙田幼兒學校';
 UPDATE PreNursery set applicationDateText='9月9日 - 9月11日索取報名表. 報名日期: 2015年9月12日' , applicationDate='2015-09-12' WHERE id=365 AND name='蘇浙小學校(本地課程)';
+UPDATE PreNursery set openDayText='簡介會 8月22日, 2015' WHERE id=494;
+
+UPDATE PreNursery set applicationDateText='2015年10月31日截止', applicationDate='2015-10-31' WHERE id=298;
+UPDATE PreNursery set applicationDateText='索取報名表: 2015年 9月11日(9-11am & 2-4pm), 9月12日(9am-12pm). 遞交報名表: 2015年 9月19日(9am-12pm), 9月21日(9-11am & 2-4pm)', applicationDate='2015-09-21' WHERE name like '%方方樂趣%';
+UPDATE PreNursery set applicationDateText='2015年8月1日至10月31日遞交報名申請', applicationDate='2015-10-31' WHERE name like '%啟思%';
+
+UPDATE PreNursery set openDayText='簡介會 2016年1月23日',applicationDateText='2015年11月2日 - 11月16日 3pm止',applicationDate='2015-11-16' WHERE id=43 AND name='九龍靈糧堂幼兒園';
+UPDATE PreNursery set applicationDateText='2015年9月4日 9pm開始 (只接受網上報名)' , applicationDate='2015-09-04' WHERE id=36 AND name='藍田靈糧幼稚園';
+UPDATE PreNursery set applicationDateText='2015年10月26日 - 10月31日 12:30pm止',applicationDate='2015-10-31' WHERE id=392 AND name='聖三一中心幼稚園';
+UPDATE PreNursery set applicationDateText='2015年9月30日止',applicationDate='2015-09-30' WHERE id=34 AND name='聖若望英文書院';
+UPDATE PreNursery set applicationDateText='2016年1月1日9:30am - 1月15日11:55pm',applicationDate='2016-01-15' WHERE id=148 AND name='基督教中心幼稚園';
+
+UPDATE PreNursery set applicationDateText='索取報名表: 2015年9月29日 - 10月17日. 報名日期: 2015年10月14日 - 10月17日',applicationDate='2015-10-17' WHERE id=349 AND name='合一堂單家傳紀念幼稚園';
+UPDATE PreNursery set applicationDateText='2015年9月14日 - 9月18日(10am-5pm)',applicationDate='2015-09-18' WHERE name like '朗思國際%';
+UPDATE PreNursery set applicationDateText='2015年8月10日 - 10月23日',applicationDate='2015-10-23' WHERE id=35 and name='偉思幼兒園';
+
+UPDATE PreNursery set applicationDateText='派發報名表: 2015年9月5日. 報名日期: 2015年9月19日 - 9月23日', applicationDate='2015-09-23' WHERE name like'嘉德麗幼稚園%';
+
